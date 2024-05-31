ee2d0f1dc691e8778d36bdd3db103462cab3e309
Roberto Loss <robertoloss@gmail.com>
2024-05-30 20:15:35 -0400

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,37 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import "./globals.css"
+import { Inter as FontSans } from "next/font/google"
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+import { cn } from "@/lib/utils"
+import React from "react"
+import { ThemeProvider } from "@/components/theme-provider"
 
-export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode}) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<main className="min-h-screen flex flex-col items-center">
+						{children}
+					</main>
+				</ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
+
